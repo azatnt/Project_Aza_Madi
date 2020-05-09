216fab7efa9f475e7230f1d04fe85d99f8bc1ea6
huistowns <huistowns@gmail.com>
2020-05-09 12:38:33 +0600

    Сделана полностью карточка, слайдер тоже сделан

--- Project/Main/static/js/main.js
+++ Project/Main/static/js/main.js
@@ -1,15 +1,7 @@
-let searchFood = document.getElementsByName("search-food")[0];
-let getInfo = searchFood.value;
+let getSliderIcons = document.getElementsByClassName("cart-promo");
 
-searchFood.addEventListener("mouseleave", () => {
-    let searchIcon = document.querySelector(".search-icon");
-    searchIcon.style.display = "block";
-});
+for (let i = 0; i < getSliderIcons.length; i++) { // Берем из БД URL и вставляем в BG image
+    let getUrl = getSliderIcons[i].getAttribute("data-url");
+    getSliderIcons[i].style.backgroundImage =  "url(" + getUrl + ")";
+}
 
-searchFood.addEventListener("mousemove", () => {
-    let searchIcon = document.querySelector(".search-icon");
-    if (getInfo.length > 0) {
-        searchIcon.style.display = "none";
-    }
-
-});
