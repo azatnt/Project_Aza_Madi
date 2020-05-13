20bb88164e8827a0cd8e23598aaea8d2744969af
pr1nce07 <58590243+pr1nce07@users.noreply.github.com>
2020-05-13 16:25:47 +0600

    Dobavil name of restaurant's is ispravil carts

--- Project/Main/Miri/urls.py
+++ Project/Main/Miri/urls.py
@@ -13,10 +13,15 @@ from Cart.views import *
 
 urlpatterns = [
 	path('', restaurant_list, name='restaurant_list_url'),
-	path('restaurant/<str:slug>', restaurant_detail, name='restaurant_detail_url'),
+	path('<str:slug>', restaurant_detail, name='restaurant_detail_url'),
+	# path('restaurants/<str:name>', rest_name, name='rest_name_url'),
+	# url(r'^restaurant/(?P<name>\w+)/(?P<slug>\w+)/$', restaurant_detail, name='restaurant_detail_url'),
 	path('category/<slug:slug>', category_detail, name='category_detail_url'),
 	path('cart/<str:id>\d+', remove_from_cart, name = 'remove_cart_url'),
 	path('cart/<str:slug>/', update_cart, name = 'update_cart_url'),
 	path('cart/', cart, name='cart_url'),
+
+
+
 
 ]
